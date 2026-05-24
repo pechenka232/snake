2f1442c566c5fb3a314c2177bb05612a8f23c2d9
pechenka232 <strekalovskijdemid@gmail.com>
2026-05-24 15:13:35 +0800

    Update lib.rs

--- zk_snake_proof/src/lib.rs
+++ zk_snake_proof/src/lib.rs
@@ -5,22 +5,22 @@ extern crate alloc;
 use core::panic::PanicInfo;
 use alloc::vec::Vec;
 
-// Устанавливаем глобальный аллокатор (для работы с памятью)
+
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// Хендлер паники (обязательно для no_std)
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// 🟢 Функция проверки очков
+#Проверка очков
 #[no_mangle]
 pub extern "C" fn verify_score(score: u32) -> u32 {
     if score > 0 {
-        1 // True
+        1 
     } else {
-        0 // False
+        0 
     }
 }
